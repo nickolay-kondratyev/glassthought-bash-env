fe3a11028c54042b86eb4943f8ee04ac0daf8b69
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2023-04-09 15:48:40 +0700

    Simplifying 1_print.sh file as there were some issues with CTRL+A functionality suspected to terminal resets

--- modules/1_print.sh
+++ modules/1_print.sh
@@ -21,20 +21,20 @@ export UNDERLINE_OFF=$(tput rmul)
 declare_print_funcs(){
   declare -A map_style_to_styleVal
 
-  map_style_to_styleVal["plain"]=""
   map_style_to_styleVal["red"]="${color_red:?}"
   map_style_to_styleVal["green"]="${color_green:?}"
   map_style_to_styleVal["yellow"]="${color_yellow:?}"
   map_style_to_styleVal["blue"]="${color_blue:?}"
   map_style_to_styleVal["purple"]="${color_purple:?}"
   map_style_to_styleVal["cyan"]="${color_cyan:?}"
-  map_style_to_styleVal["underline"]="${underline_on:?}"
+  # map_style_to_styleVal["plain"]=""
+  # map_style_to_styleVal["underline"]="${underline_on:?}"
 
   for style in "${!map_style_to_styleVal[@]}"; do
     local style_val="${map_style_to_styleVal[$style]}"
 
     eval "echo_${style}() {
-      echo \"${style_val}\${@}${color_reset}${underline_off}\"
+      echo \"${style_val}\${@}${color_reset}\"
     }
     export -f echo_${style}
 
@@ -44,7 +44,7 @@ declare_print_funcs(){
     export -f echo.${style}
 
     print_${style}() {
-      echo -n \"${style_val}\${@}${color_reset}${underline_off}\"
+      echo -n \"${style_val}\${@}${color_reset}\"
     }
     export -f print_${style}
 
@@ -57,4 +57,14 @@ declare_print_funcs(){
 }
 declare_print_funcs
 
+
+print.plain() {
+  echo -n "${@}"
+}
+export -f print.plain
+
+echo.plain() {
+  echo "${@}"
+}
+export -f echo.plain
 
